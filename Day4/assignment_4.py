c81a93c49f01c9971629ef5753920fd7073f72e4
Khushigohel <gohelkhushi2004@gmailcom>
2026-01-06 16:45:27 +0530

    Complete Day 3 internship tasks

---
+++ Day4/assignment_4.py
@@ -0,0 +1,23 @@
+# 1. Write a program that takes two numbers as input from the user and displays their sum.
+n1 = int(input("Enter the number1:"))
+n2= int(input("Enter the number2:"))
+
+ans=n1 + n2 
+print("sum f two number is:",ans)
+
+#2. Write a program that takes a string as input from the user and displays it in uppercase letters.
+input_str=input("Enter the String:")
+print("String  is:",input_str.upper())
+
+# 3. Write a program that displays the following text, using triple quotes: Programming is fun. I love Python.
+text='''Programming is fun. 
+I love Python.'''
+print(text)
+
+# 4.  Write a program that displays the following text, using escape characters:She said, "Hello, world!"
+print("She said, \"Hello, world!\"")
+
+# 5.  Write a program that takes a sentence as input from the user and displays the number of words in the sentence.
+text=input("Enter the text")
+count_txt=len(text.split())
+print(count_txt)
---
+++ Day4/book_Inventory_Management.py
@@ -0,0 +1,64 @@
+''' You are managing the inventory of a bookstore. 
+You need to create a Python program that allows you to 
+add new books to the inventory and display the current 
+inventory.
+
+ Program should contain add_book function which 
+ask user name of book and in second line quantity 
+of book. Add it in inventory dictionary
+■ display_inventory: Print all books currently in dict
+■ update_book_quantity: input book_name and 
+updated quantity and update in dictionary.
+■ Application should keep running until user wants 
+to exit
+'''
+inventory_store={}
+
+def add_book():
+    try:
+        book_name=input("Enter the book name:")
+        quantity=int(input("How many quantity take to buy..."))
+        inventory_store[book_name]=quantity
+        print("\n Book Add Sucessfully....")
+    except ValueError:
+        print("Quantity must be a number. Please try again.")
+        
+
+def display_book():
+    if not  inventory_store:
+        print("\n Book is Not Avaliable")
+    else:
+        print("Book_Name    quantity")
+        print("-"*35)
+        for book_name,quantity in inventory_store.items():
+            print(book_name, " ", quantity)
+            print("\n ")
+
+def update_book_quantity():
+    book_name=input("Enter the book name to update the quantity:")
+    if book_name in inventory_store:
+        quantity_update=int(input("How many quantity take to update..."))
+        inventory_store[book_name]=quantity_update
+        print("Book quantity updated successfully.\n")
+    else:
+        print("\n Book not found in inventory.")
+    
+
+while True:
+    print("Book Invetory Management:")
+    print("\n 1. Add the Book")
+    print("\n 2. Display the Book")
+    print("\n 3. Update the Book Quantity")
+    print("\n 4. Exit")
+    
+    choice=int(input("\n Enter the choice:"))
+    if choice == 1:
+        add_book()
+    elif choice == 2:
+        display_book()
+    elif choice == 3:
+        update_book_quantity()
+    elif choice == 4:
+        exit()
+    else:
+        print("Invalid choice....")

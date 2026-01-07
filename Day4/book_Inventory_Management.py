17c3343b9de98833f5b7ff16fbcc23b8357d5210
Khushigohel <gohelkhushi2004@gmailcom>
2026-01-07 14:05:43 +0530

    book inventory management is perform useing class

--- Day4/book_Inventory_Management.py
+++ Day4/book_Inventory_Management.py
@@ -12,37 +12,40 @@ updated quantity and update in dictionary.
 ■ Application should keep running until user wants 
 to exit
 '''
-inventory_store={}
+class Book_Inventory:
+    def __init__(self):
+        self.inventory_store={}
 
-def add_book():
-    try:
-        book_name=input("Enter the book name:")
-        quantity=int(input("How many quantity take to buy..."))
-        inventory_store[book_name]=quantity
-        print("\n Book Add Sucessfully....")
-    except ValueError:
-        print("Quantity must be a number. Please try again.")
+    def add_book(self):
+        try:
+            book_name=input("Enter the book name:")
+            quantity=int(input("How many quantity take to buy..."))
+            self.inventory_store[book_name]=quantity
+            print("\n Book Add Sucessfully....")
+        except ValueError:
+            print("Quantity must be a number. Please try again.")
+            
+
+    def display_book(self):
+        if not  self.inventory_store:
+            print("\n Book is Not Avaliable")
+        else:
+            print("Book_Name    quantity")
+            print("-"*35)
+            for book_name,quantity in self.inventory_store.items():
+                print(book_name, " ", quantity)
+                print("\n ")
+
+    def update_book_quantity(self):
+        book_name=input("Enter the book name to update the quantity:")
+        if book_name in self.inventory_store:
+            quantity_update=int(input("How many quantity take to update..."))
+            self.inventory_store[book_name]=quantity_update
+            print("Book quantity updated successfully.\n")
+        else:
+            print("\n Book not found in inventory.")
         
-
-def display_book():
-    if not  inventory_store:
-        print("\n Book is Not Avaliable")
-    else:
-        print("Book_Name    quantity")
-        print("-"*35)
-        for book_name,quantity in inventory_store.items():
-            print(book_name, " ", quantity)
-            print("\n ")
-
-def update_book_quantity():
-    book_name=input("Enter the book name to update the quantity:")
-    if book_name in inventory_store:
-        quantity_update=int(input("How many quantity take to update..."))
-        inventory_store[book_name]=quantity_update
-        print("Book quantity updated successfully.\n")
-    else:
-        print("\n Book not found in inventory.")
-    
+Inventory_system=Book_Inventory()
 
 while True:
     print("Book Invetory Management:")
@@ -50,14 +53,14 @@ while True:
     print("\n 2. Display the Book")
     print("\n 3. Update the Book Quantity")
     print("\n 4. Exit")
-    
+        
     choice=int(input("\n Enter the choice:"))
     if choice == 1:
-        add_book()
+        Inventory_system.add_book()
     elif choice == 2:
-        display_book()
+        Inventory_system.display_book()
     elif choice == 3:
-        update_book_quantity()
+        Inventory_system.update_book_quantity()
     elif choice == 4:
         exit()
     else:
